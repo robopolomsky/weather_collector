16124f9102d097741325066800be12e340a217d7
Robert Polomsky <robert.polomsky@autodesk.com>
2016-06-09 10:55:35 +0200

    WeatherCollector - version 1.0

---
+++ WeatherCollector/CityInfo.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+using NLog;
+
+namespace WeatherCollector
+{
+    class CityInfo
+    {
+        public bool Loaded { get; private set; }
+
+        private string Town
+        {
+            get
+            {
+                return _town ?? _cityID;
+            }
+            set { _town = value; }
+        }
+
+        private readonly string _cityID;
+
+        public CityInfo(string cityID)
+        {
+            _cityID = cityID;
+        }
+
+        private string _temperature;
+        private string _condition;
+        private string _humidity;
+        private string _windSpeed;
+        private string _town;
+        private Exception _lastErr;
+
+
+        public void LoadWeather()
+        {
+            Console.Out.Write($"citam mesto c. { _cityID} ... ");
+            const string unit = "&u=c"; // celsius
+            const string cKey = "add yahoo key here";
+            const string cSecret = "add yahoo secret here";
+
+            var baseURL = string.Format($"http://weather.yahooapis.com/forecastrss?w={_cityID}{unit}");
+
+            string urlDes, Params;
+            var oAuth = new OAuthBase();
+            var signature = oAuth.GenerateSignature(new Uri(baseURL), cKey, cSecret, "", "", "GET", oAuth.GenerateTimeStamp(), oAuth.GenerateNonce(), out urlDes, out Params);
+
+            var wc = new WebClient();
+            try
+            {
+                string downloadString = wc.DownloadString(new Uri($"{urlDes}?{Params}&oauth_signature={signature}"));
+
+                //XDocument xdoc = XDocument.Parse(downloadString, LoadOptions.None);
+                //xdoc.Save("c:\\temp\\weather.xml");
+                //Console.Out.WriteLine(xdoc.FirstNode);
+
+                XmlDocument wData = new XmlDocument();
+                wData.LoadXml(downloadString);
+
+                XmlNamespaceManager manager = new XmlNamespaceManager(wData.NameTable);
+                manager.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+                XmlNode channel = wData.SelectSingleNode("rss").SelectSingleNode("channel");
+
+                _temperature = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["temp"].Value;
+                _condition = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["text"].Value;
+                _humidity = channel.SelectSingleNode("yweather:atmosphere", manager).Attributes["humidity"].Value;
+                _windSpeed = channel.SelectSingleNode("yweather:wind", manager).Attributes["speed"].Value;
+                Town = channel.SelectSingleNode("yweather:location", manager).Attributes["city"].Value;
+
+                Console.Out.WriteLine(Message());
+                Loaded = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($" ERROR: {e.Message}");
+                _lastErr = e;
+                Loaded = false;
+            }
+        }
+
+        public void Output()
+        {
+            LogEventInfo logEventInfo;
+            if (Loaded)
+                logEventInfo = new LogEventInfo
+                {
+                    Level = LogLevel.Info,
+                    Message = Message()
+                };
+            else
+            {
+                logEventInfo = new LogEventInfo
+                {
+                    Level = LogLevel.Error,
+                    Message = string.Format($"ID Mesta: {_cityID} Chyba: {_lastErr.Message}")
+                };
+            }
+            LogManager.GetCurrentClassLogger().Log(logEventInfo);
+            Console.Out.WriteLine(logEventInfo.Message);
+        }
+
+        private string Message()
+        {
+            return $"Mesto: {Town}, Teplota: {_temperature}°C, Vlhkost: {_humidity}%, Vietor: {_windSpeed}km/h, {_condition}";
+        }
+    }
+}
---
+++ WeatherCollector/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace WeatherCollector
+{
+    class Program
+    {
+        private static List<CityInfo> _cities;
+
+        static void Main()
+        {
+            _cities = new List<CityInfo>
+            {
+                new CityInfo("820323"), // Kosice
+                new CityInfo("821722"), // Poprad
+                new CityInfo("821782"), // Presov
+                new CityInfo("819877"), // Humenne
+                new CityInfo("818511"), // Banska Bystrica
+                new CityInfo("818717"), // Bratislava
+                new CityInfo("823382") // Zilina
+            };
+            Console.Out.WriteLine($"Zistujem akutalne pocasie v {_cities.Count} mestach ...");
+
+            int loadAttempts = 0;
+            const int maxLoadAttempts = 200;
+            while (_cities.Any(x => !x.Loaded) && loadAttempts < maxLoadAttempts)
+            {                
+                foreach (CityInfo city in _cities.Where(x => !x.Loaded))
+                {
+                    loadAttempts++;
+                    city.LoadWeather();
+                }
+
+                if(_cities.Any(x => !x.Loaded))
+                    Thread.Sleep(5000); // 5 sec
+            }
+            if (loadAttempts >= maxLoadAttempts)
+            {
+                string errMsg = string.Format($"CHYBA: Dosiahnuty maximalny pocet {maxLoadAttempts} pokusov");
+                Console.Out.WriteLine(errMsg);
+                NLog.LogManager.GetCurrentClassLogger().Error(errMsg);
+            }
+            Console.Out.WriteLine("");
+            foreach (CityInfo city in _cities)
+                city.Output();
+
+            //Console.ReadKey();
+        }
+
+    }
+}
